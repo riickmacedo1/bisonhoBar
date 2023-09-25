96e28e19b0a96cd053bb46bcbed1909ce7be8a58
riickmacedo1 <riickmacedo1@gmail.com>
2023-09-25 00:15:21 -0300

    todas as funcionalidades finalizadas

--- src/controllers/product/CreateProductController.ts
+++ src/controllers/product/CreateProductController.ts
@@ -4,18 +4,24 @@ import { CreateProductService } from "../../services/product/CreateProductServic
 class CreateProductController {
   async handle(req: Request, res: Response) {
     const { name, price, description, category_id } = req.body;
-    let banner = "";
 
     const createProductService = new CreateProductService();
-    const product = await createProductService.execute({
-      name,
-      price,
-      description,
-      banner,
-      category_id,
-    });
 
-    return res.json(product);
+    if (!req.file) {
+      throw new Error("error upload file");
+    } else {
+      const { originalname, filename: banner } = req.file;
+
+      const product = await createProductService.execute({
+        name,
+        price,
+        description,
+        banner,
+        category_id,
+      });
+
+      return res.json(product);
+    }
   }
 }
 
